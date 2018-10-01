11effcb590bbd3d02d4273b1419d990a13021e48
SEUNGMIN KIM <smk950226@naver.com>
2018-10-01 23:41:04 +0900

    변환된 bundle 생성

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,16 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/, //정규표현식, js파일을 찾는다.
+                loader: 'babel-loader', //js파일 찾으면 이 로더 사용
+                options: { //babel의 preset지정 등 옵션
+                    presets: ['@babel/preset-env']
+                }
+            }
+        ]
     }
 }
